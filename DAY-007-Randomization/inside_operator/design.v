54c394c286d0747fb097be5b028513c4d9e7f33a
Divya Sai Iruku <137642497+Divya-i@users.noreply.github.com>
2023-09-19 21:04:20 +0530

    D-7

--- DAY-007-Randomization/inside_operator/design.v
+++ DAY-007-Randomization/inside_operator/design.v
@@ -5,8 +5,12 @@ module tb;
     
     randc  bit [3:0] a;
     randc  bit [2:0] b;
+    rand   bit [0:4] c;
+
     constraint c1 { a inside {[0:2],4,6,8};}
     constraint c2 { b inside {[0:3],5,7};}
+    constraint c3 {!(c inside {[0:2],3,4});}
+
      
   endclass
   
@@ -17,7 +21,7 @@ module tb;
     repeat(6)
       begin 
         a1.randomize();
-        $display(" value of a : %0d, b : %0d", a1.a, a1.b);
+        $display(" value of a : %0d, b : %0d, c : %0d", a1.a, a1.b,a1.c);
       end
   end
 endmodule 
