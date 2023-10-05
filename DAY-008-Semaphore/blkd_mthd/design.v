a52aa0cecbedc5d29dfa26354b2f2448b9736c5c
Divya Sai Iruku <137642497+Divya-i@users.noreply.github.com>
2023-10-05 10:30:16 +0530

    D-8

---
+++ DAY-008-Semaphore/blkd_mthd/design.v
@@ -0,0 +1,25 @@
+// Code your design here
+module semaphore_ex;
+  class A;
+    semaphore s_1 = new(6);
+    
+    task automatic display(int key);
+      s_1.get(key);
+      $display(" current simualtion time : %0t , got %0d keys", $time, key);
+      #60;
+      s_1.put(key);
+    endtask
+  endclass
+  
+  initial begin 
+    A obj = new();
+    
+    fork
+      obj.display(3);
+      obj.display(3);
+      obj.display(2);
+      obj.display(4);
+    join
+  end
+endmodule
+      
